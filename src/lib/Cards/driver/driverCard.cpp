89f9754b3c15444b29dcef08abe9bb4e48c4383a
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-03-03 20:22:34 +0700

    Update driverCard.cpp

--- src/lib/Cards/driver/driverCard.cpp
+++ src/lib/Cards/driver/driverCard.cpp
@@ -6,6 +6,7 @@ int main(){
     Card kartu1;
     Card kartu2(3,"hijau");
     Card kartu3("quadruple");
+    Card kartu4(3,"hijau");
 
     cout<<"Color kartu1: "<<kartu1.getColor()<<endl;
     cout<<"Category kartu1: "<<kartu1.getCategory()<<endl;
@@ -22,6 +23,11 @@ int main(){
 
     kartu1.getColor();    
 
+    if(kartu2==kartu4){
+        cout<<"kartu pertama sama dengan kartu kedua"<<endl;
+    } else {
+        cout<<"kartu pertama tidak sama dengan kartu kedua"<<endl;
+    }
 
     return 0;
 }
