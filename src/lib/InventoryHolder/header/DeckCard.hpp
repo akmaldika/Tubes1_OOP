bff7639864c0ac2e3998361d8ee2d5236d7bc002
Muhammad Fadhil Amri <fadhil133113@gmail.com>
2023-03-03 23:58:22 +0700

    Resolver conflict InventoryHolder #2

--- src/lib/InventoryHolder/header/DeckCard.hpp
+++ src/lib/InventoryHolder/header/DeckCard.hpp
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-#ifndef __DECK_CARD_HPP__
-#define __DECK_CARD_HPP__
-
-#include <iostream>
-#include "InventoryHolder.hpp"
-
-using namespace std;
-
-// Class DeckCard
-class DeckCard : public InventoryHolder{
-    public:
-        // Attribute
-        vector<Card> deckCard;
-        static int deckCardCount;
-
-        // Constructor
-        DeckCard(vector<Card> deckCard);
-
-        // Copy Constructor
-        DeckCard(const DeckCard& deckCard);
-
-        // Destructor
-        ~DeckCard();
-
-        // Setter and Getter
-        void setDeckCard(vector<Card> deckCard);
-        vector<Card> getDeckCard();
-        static int getDeckCardCount();
-
-        // Method
-        DeckCard& operator+(Card card);     // Operator Overloading (add card)
-        DeckCard& operator-(Card card);     // Operator Overloading (remove card)
-        void printCard();
-};
-
-=======
 #ifndef __DECK_CARD_HPP__
 #define __DECK_CARD_HPP__
 
@@ -72,5 +35,4 @@ class DeckCard : public InventoryHolder{
         void printCard();
 };
 
->>>>>>> 95786990c5ba5c95a3f1d0bab2e4b8e875713d27
 #endif
--- src/lib/InventoryHolder/header/TableCard.hpp
+++ src/lib/InventoryHolder/header/TableCard.hpp
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-#ifndef __TABLE_CARD_HPP__
-#define __TABLE_CARD_HPP__
-
-#include <iostream>
-#include "InventoryHolder.hpp"
-
-using namespace std;
-
-// Class TableCard
-class TableCard : public InventoryHolder{
-    public:
-        // Attribute
-        vector<Card> tableCard;
-        static int tableCardCount;
-
-        // Constructor
-        TableCard(vector<Card> tableCard);
-
-        // Copy Constructor
-        TableCard(const TableCard& tableCard);
-
-        // Destructor
-        ~TableCard();
-
-        // Setter and Getter
-        void setTableCard(vector<Card> tableCard);
-        vector<Card> getTableCard();
-        static int getTableCardCount();
-
-        // Method
-        TableCard& operator+(Card card);     // Operator Overloading (add card)
-        TableCard& operator-(Card card);     // Operator Overloading (remove card)
-        void printCard();
-};
-
-=======
 #ifndef __TABLE_CARD_HPP__
 #define __TABLE_CARD_HPP__
 
@@ -71,5 +34,4 @@ class TableCard : public InventoryHolder{
         void printCard();
 };
 
->>>>>>> 95786990c5ba5c95a3f1d0bab2e4b8e875713d27
 #endif
