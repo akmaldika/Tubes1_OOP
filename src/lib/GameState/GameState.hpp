90dc2ab9bec6b62dc764a78b8b60f758725bead4
Naufal Firdaus <naufalfirdaus21@gmail.com>
2023-03-03 15:46:47 +0700

    Header for GameEngine & GameState

--- src/lib/GameState/GameState.hpp
+++ src/lib/GameState/GameState.hpp
@@ -1,6 +1,8 @@
 #include "Ability.hpp"
 #include "Player.hpp"
-
+#include "InventoryHolder/header/TableCard.hpp"
+#include <vector>
+#include <iostream>
 
 /*
     KELAS GAMESTATE
@@ -10,13 +12,64 @@
 class GameState {
 
     private:
-        Player *AllPlayer;  // array semua player yang terurut berdasarkan gilirannya
+    // ATTRIBUTE
+        vector<Player> AllPlayer;  // array semua player yang terurut berdasarkan gilirannya
         int PrizePool;  // poin hadiah
-        int round; // hitungan ronde
+        int Round; // hitungan ronde
 
-        TableCard cardtable;
+        /*  TURN
+            giliran player yang mana saat ini.
+            pair->first = urutan player/ index dalam vector
+            pair->second = objek playernya
+        */
+        pair<int, Player> Turn;
+        TableCard CardTable; // kartu di meja
+
+        const int DefaultPrize = 64;
 
     public:
+    // CONSTRUCTOR
+        /*
+            Const
+            AllPlayer = diisi dengan player 1 sampai 7
+            PrizePool = diisi dengan default
+            Round = diisi dengan ronde satu
+            CardTable = instantiasi TableCard dengan jumlah 0
+        */
+        GameState();
+
+    // Get Method
+        int getPrize();
+        int getRound();
+        TableCard getTableCard();
+        Player getPlayer(int ID); // setiap player ada id 1-7 yang terpisah dari urutannya.
+        pair<int,Player> getWhoseTurn(); // return giliran siapa skrg
+
+    // Set Method
+        void setPrize(int amount);
+
+    
+    // Method
+        /*
+            round + 1
+            Tambah card diatas table
+            urutan pemain diubah
+
+        */
+        void NextRound();
+        /*
+            Turn diganti sesuai urutan di vector
+        */
+        void NextTurn();
+
+        void AddCardToTable(Card cardAdded); // tambah kartu di meja
+
+        void printState();
+
+
+        
+
+        
 
 
 };
