7716d3355215f113e62563673b515f9112efa147
4kmalm <16521070@mahasiswa.itb.ac.id>
2023-03-04 04:33:28 +0700

    Update Player.hpp

---
+++ src/lib/Player/Player.hpp
@@ -0,0 +1,43 @@
+#ifndef __PLAYER__
+#define __PLAYER__
+
+#include "../Cards/header/Card.hpp"
+// #include "../Ability/header/Ability.hpp"
+#include "../InventoryHolder/header/PlayerCard.hpp"
+
+#include <iostream>
+using namespace std;
+
+class Player {
+    private:
+    int ID; // id satu player yang diambil dari playerCount -Naufal
+    string name;
+    int point;  // point kayanya gapake static deh, biar tiap player bisa
+                // punya point berbeda -Naufal
+    PlayerCard MyCard;
+
+    static int playerCount;
+
+    public:
+    Player(); // temp biar gameState ga error
+    Player(Card card1, Card card2, Card abilityCard);
+
+    // Player(string name);
+    void setPlayerName(string name);
+    string getPlayerName();
+
+    void setPlayerPoint(int point);
+    int getPlayerPoint();
+
+    // void addOne(Card one);
+    // void addTwo(Card two);
+    Card getCardOne();
+    Card getCardTwo();
+
+    void pointDouble();
+    void pointHalf();
+    void playerNext();
+    void highestCombination();
+};
+
+#endif
