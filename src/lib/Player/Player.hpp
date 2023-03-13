a88658ecd60c23b9e8033655ce15a0cf0e87a0a9
Satria Octavianus Nababan <88760287+satrianababan@users.noreply.github.com>
2023-03-13 09:28:51 +0700

    coba

--- src/lib/Player/Player.hpp
+++ src/lib/Player/Player.hpp
@@ -2,7 +2,7 @@
 #define __PLAYER__
 
 #include "../Cards/header/Card.hpp"
-#include "../Ability/Ability.hpp"
+#include "../AbilityCard/header/AbilityCard.hpp"
 #include "../InventoryHolder/header/PlayerCard.hpp"
 
 #include <iostream>
