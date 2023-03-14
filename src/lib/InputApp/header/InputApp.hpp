323490a9dd7a3181576bd54312f7852f8fee1ece
4kmalm <16521070@mahasiswa.itb.ac.id>
2023-03-14 23:26:34 +0700

    add destructor PlayerCard

--- src/lib/InputApp/header/InputApp.hpp
+++ src/lib/InputApp/header/InputApp.hpp
@@ -17,18 +17,32 @@ private:
     int intInput;
 
 public:
-    /// @brief Default constructor
+    // constructor
     InputApp();
     InputApp(string,int);
+    InputApp(const InputApp&);
     
-    
+    // getter and setter
     string getStrInput() const;
     int gerIntInput() const;
     void setStrInput(string str);
     void setIntInput(int i);
 
+    /* 
+    Meminta Masukan berupa string
+    */
     void takeStrInput();
+
+    /* 
+    Meminta masukan berupa int bebas
+     */
     void takeIntInput();
+
+    /* 
+    Meminta masukan berupa integer dengan batas 1 sampai nmaks 
+    throw "Input out of range" jika diluar batas nmaks
+    throw "Input must be a number from 1 to "+ to_string(nmaks) jika input bukan integer
+    */
     void takeIntInput(int nmkas);
 }; 
 
